b5bce47f950b66fc4d5cdf1b7ead26dab285d249
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-03-05 09:05:54 -0300

    feat: add test for departament repository

--- tests/repositories/departament-repository.spec.ts
+++ tests/repositories/departament-repository.spec.ts
@@ -1,7 +1,7 @@
 import { PrismaClient, Departament } from "@prisma/client";
 import { ObjectId } from "mongodb";
 import { IDepartament } from "../domain/interfaces/interfaces";
-import { CREATE_DEPARTAMENT } from "../mock/mocks";
+import { CREATE_DEPARTAMENT } from "../config/mock/mocks";
 import { IRepository } from "./interfaces/repository";
 
 export class DepartamentRespository implements IRepository {
@@ -19,9 +19,22 @@ export class DepartamentRespository implements IRepository {
 }
 
 describe("DepartamentRepository", () => {
+  let client: any;
+  // beforeAll(async () => {
+  //   client = await startDatabase();
+  // });
   it("should return a departament", async () => {
-    const departament = new DepartamentRespository(new PrismaClient());
 
+    const a =  {
+      id: '640381faac89773044d6e1b3',
+      name: 'Juridic',
+      team: [ 'Anderson', 'Armando', 'Hector' ],
+      chief: 'John Doe',
+      createdAt: new Date(`2023-03-04T17:38:02.768Z`),
+      deletedAt: null,
+      updatedAt: null
+    }
+    const departament = new DepartamentRespository(new PrismaClient());
     await departament.create(CREATE_DEPARTAMENT);
 
     expect(1).toBe(1);
